3ed0dddf5701c820d28603178fa4ccea7ee80832
Everton Renan <everton.renan@clevertech.biz>
2023-02-28 16:37:04 -0300

    chore: build for QA

---
+++ react-app/src/views/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Example/Home";
+export { default as ExampleUI } from "./Example/ExampleUI";
+export { default as Hints } from "./Example/Hints";
+export { default as StormContract } from "./StormContract";
+export { default as Subgraph } from "./Example/Subgraph";
